b6df31a54e19d0b9039e1cfc288bb4ccafc74737
Jay Kamat <github@jgkamat.33mail.com>
2016-02-05 09:19:01 -0500

    apply magically to alda files and add highlighting to octaves/bars

--- alda-mode.el
+++ alda-mode.el
@@ -9,8 +9,10 @@
     (alda-voice-regexp "\\([Vv][0-9]+\\):")
     (alda-string-regexp "“\\([^ ]+?\\)”")
     (alda-timing-regexp "[a-gr]\\([~.0-9/]*\\)")
-    (alda-pitch-shift "[a-gr]\\([\-\+]+\\)")
+    (alda-pitch-shift "[a-gr]\\([-+]+\\)")
+    (alda-bar-regexp "\\(|\\)")
     (alda-set-octave-regexp "\\(o[0-9]+\\)")
+    (alda-shift-octave-regexp "\\(>\\|<\\)")
     )
 
   (setq alda-highlights
@@ -19,8 +21,10 @@
        (,alda-instrument-regexp . (1 font-lock-function-name-face))
        (,alda-string-regexp . (1 font-lock-string-face))
        (,alda-set-octave-regexp . (1 font-lock-type-face))
+       (,alda-shift-octave-regexp . (1 font-lock-type-face))
        (,alda-timing-regexp . (1 font-lock-builtin-face))
        (,alda-pitch-shift . (1 font-lock-preprocessor-face))
+       (,alda-bar-regexp . (1 font-lock-preprocessor-face))
        )))
 
 (define-derived-mode alda-mode fundamental-mode
@@ -31,5 +35,8 @@
   ;; Set alda highlighting
   (setq font-lock-defaults '(alda-highlights))
   (setq mode-name "Alda"))
+
+;; Open alda files in alda-mode
+(add-to-list 'auto-mode-alist '("\\.alda\\'" . alda-mode))
 
 (provide 'alda-mode)
