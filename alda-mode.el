e26a037d7c770d1f975f6940560db7c8659c5aaf
Jay Kamat <github@jgkamat.33mail.com>
2016-02-05 02:51:14 -0500

    Do a bit of cleanup and improve consistency

--- alda-mode.el
+++ alda-mode.el
@@ -4,9 +4,9 @@
 ;;; Regexes
 (let
   ;; Prevent regexes from taking up memory
-  ((alda-comment-regexp "#.*$")
+  ((alda-comment-regexp "\\(#.*$\\)")
     (alda-instrument-regexp "^\\([a-zA-Z]\\{2\\}[A-Za-z0-9_\-]*\\)\\(\s\\(\"[A-Za-z0-9_\-]*\"\\)\\)?:")
-    (alda-voice-regexp "\\([Vv][0-9]+:\\)")
+    (alda-voice-regexp "\\([Vv][0-9]+\\):")
     (alda-string-regexp "“\\([^ ]+?\\)”")
     (alda-timing-regexp "[a-gr]\\([~.0-9/]*\\)")
     (alda-pitch-shift "[a-gr]\\([\-\+]+\\)")
@@ -14,8 +14,8 @@
     )
 
   (setq alda-highlights
-    `((,alda-comment-regexp . font-lock-comment-face)
-       (,alda-voice-regexp . (0 font-lock-function-name-face))
+    `((,alda-comment-regexp . (1 font-lock-comment-face) )
+       (,alda-voice-regexp . (1 font-lock-function-name-face))
        (,alda-instrument-regexp . (1 font-lock-function-name-face))
        (,alda-string-regexp . (1 font-lock-string-face))
        (,alda-set-octave-regexp . (1 font-lock-type-face))
