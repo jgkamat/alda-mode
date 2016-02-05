dab3b9e39195169886f585d8d8430bc0a3d8cfe7
Jay Kamat <github@jgkamat.33mail.com>
2016-02-05 12:46:51 -0500

    Use asm-mode indention, and change a few colors

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# alda-mode
-An Alda Major Mode for Emacs
+# Alda Mode
+
+An [Alda](https://github.com/alda-lang/alda) Major Mode for Emacs
+
+Alda mode is a simple mode to provide syntax highlighting and other basic language features for [Alda](https://github.com/alda-lang/alda), the music programming language written in clojure!
+
+This project is currently under construction. Come back when it's done!
--- alda-mode.el
+++ alda-mode.el
@@ -1,11 +1,13 @@
 
 ;;;; alda-mode for emacs. A simple major mode that provides language features for alda
 
+(require 'asm-mode)
+
 ;;; Regexes
 (let
   ;; Prevent regexes from taking up memory
   ((alda-comment-regexp "\\(#.*$\\)")
-    (alda-instrument-regexp "^\\([a-zA-Z]\\{2\\}[A-Za-z0-9_\-]*\\)\\(\s\\(\"[A-Za-z0-9_\-]*\"\\)\\)?:")
+    (alda-instrument-regexp "\\([a-zA-Z]\\{2\\}[A-Za-z0-9_\-]*\\)\\(\s+\\(\"[A-Za-z0-9_\-]*\"\\)\\)?:")
     (alda-voice-regexp "\\([Vv][0-9]+\\):")
     (alda-string-regexp "“\\([^ ]+?\\)”")
     (alda-timing-regexp "[a-gr]\s*\\([~.0-9/]*\\)")
@@ -17,6 +19,7 @@
 
   (setq alda-highlights
     `((,alda-comment-regexp . (1 font-lock-comment-face) )
+       (,alda-bar-regexp . (1 font-lock-comment-face))
        (,alda-voice-regexp . (1 font-lock-function-name-face))
        (,alda-instrument-regexp . (1 font-lock-function-name-face))
        (,alda-string-regexp . (1 font-lock-string-face))
@@ -24,13 +27,14 @@
        (,alda-shift-octave-regexp . (1 font-lock-type-face))
        (,alda-timing-regexp . (1 font-lock-builtin-face))
        (,alda-pitch-shift . (1 font-lock-preprocessor-face))
-       (,alda-bar-regexp . (1 font-lock-preprocessor-face))
        )))
 
 (define-derived-mode alda-mode fundamental-mode
   ;; Set alda comments
   (setq-local comment-start "# ")
   (setq-local comment-end "")
+
+  (setq-local indent-line-function 'asm-indent-line)
 
   ;; Set alda highlighting
   (setq font-lock-defaults '(alda-highlights))
