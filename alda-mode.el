165dcc5734c5d2d3be7075c3a340b1532b6f3819
Jay Kamat <github@jgkamat.33mail.com>
2016-02-05 17:45:10 -0500

    Add primitive support for running alda selections in emacs Does not work if you haven't run a instrument: before, just like alda Simply pipes selection to alda play --code ''

--- alda-mode.el
+++ alda-mode.el
@@ -3,6 +3,29 @@
 
 (require 'asm-mode)
 
+(defconst alda-output-buffer "*alda-output*")
+(defconst alda-output-name "alda-playback")
+
+(defun alda-play-reigion (start end)
+  "Plays the current selection in alda"
+  (interactive "r")
+  (if (eq (length (shell-command-to-string "which alda")) 0)
+    (message "Alda was not found on your $PATH.")
+    (if (eq start end)
+      (message "No mark was set!")
+      (progn
+        (start-process-shell-command alda-output-name alda-output-buffer
+          (concat "alda play --code '" (buffer-substring-no-properties start end)
+            ;; Infinite loop, prevents emacs from killing the alda server.
+            ;; TODO: make this happen only when running the server.
+            "' &&  while true; do; sleep 1; done"))))))
+(defun alda-stop ()
+  "Stops songs from playing, and cleans up idle alda runner processes
+Because alda runs in the background, the only way to do this is with alda restart as of now."
+  (interactive)
+  (shell-command "alda stop -y")
+  (delete-process alda-output-buffer))
+
 ;;; Regexes
 (let
   ;; Prevent regexes from taking up memory
